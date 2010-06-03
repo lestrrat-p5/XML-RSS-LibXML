b07cc1f328f4d9ee5898f56f4ca25334cd70ef91
lestrrat <lestrrat+github@gmail.com>
2010-06-03 10:46:32 +0900

    properly set version

--- lib/XML/RSS/LibXML/V0_92.pm
+++ lib/XML/RSS/LibXML/V0_92.pm
@@ -6,4 +6,17 @@ use base qw(XML::RSS::LibXML::V0_91);
 # Should be compatible with 0.91.
 # See http://backend.userland.com/rss092
 
+sub parse_dom
+{
+    my $self = shift;
+    my $c    = shift;
+    my $dom  = shift;
+
+    $c->reset;
+    $c->version('0.92');
+    $self->parse_namespaces($c, $dom);
+    $self->parse_channel($c, $dom);
+    $self->parse_items($c, $dom);
+}
+
 1;
